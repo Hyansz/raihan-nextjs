d0491e2fabdd50fa702fe000771c3299b45fc5a9
Raihan Regita Harjuno <raihanregita.ppqita@gmail.com>
2023-10-25 12:05:05 +0700

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -42,6 +42,7 @@ export default function Home() {
           height={37}
           priority
         /> */}
+        <h1 className='text-7xl font-bold'>Muhammad Mufid</h1>
       </div>
 
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
