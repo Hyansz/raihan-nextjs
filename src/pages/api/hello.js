7b4eaf2638dda8c65a3304dfb654b59489a98a77
Raihan Regita Harjuno <raihanregita.ppqita@gmail.com>
2023-10-25 11:40:02 +0700

    first-commit

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,5 +1,13 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  console.log(req.method); //POST
+  console.log(req.body); //{ NIS: '11223', displayName: 'admin', password: '123456'}
+  // APP POST
+  if (req.method === 'POST') {
+    // Jalankan
+    res.status(200).json({ message: 'Hello' + req.body.displayName })
+  } else {
+    res.status(404).json({ error: true, message: 'method tidak dijalankan'})
+  }
 }
